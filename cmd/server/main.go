3c5a9c0ee404af65ce53041fdd1344185b26516f
User <email@mail.com>
2025-06-04 23:19:37 +0300

    test pass

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,55 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/skdiver33/metrics-collector/models"
+)
+
+type MetricsHandler struct{}
+
+func (handler *MetricsHandler) ServeHTTP(rw http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		http.Error(rw, "Only Post requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Println(request.URL.Path)
+	metricsData := strings.Split(request.URL.Path, "/")
+	metricsData = metricsData[2:]
+	if len(metricsData) != 3 {
+		http.Error(rw, "Not all metrics data defined!", http.StatusNotFound)
+		return
+	}
+	metricsType := metricsData[0]
+	metricsName := metricsData[1]
+	metricsValue := metricsData[2]
+
+	if strings.Compare(metricsType, models.Counter) != 0 && strings.Compare(metricsType, models.Gauge) != 0 {
+		http.Error(rw, "Wrong metrics type", http.StatusBadRequest)
+		return
+	}
+
+	if metricsName == "" {
+		http.Error(rw, "Not all metrics data defined!", http.StatusNotFound)
+		return
+	}
+
+	if _, err := strconv.Atoi(metricsValue); err != nil {
+		http.Error(rw, "Wrong metrics type", http.StatusBadRequest)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Set("Content-type", "text/plain")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/update/", &MetricsHandler{})
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		panic("Error start server")
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/skdiver33/metrics-collector
 
 go 1.24.3
+
+require github.com/stretchr/testify v1.10.0 // indirect
