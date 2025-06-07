0b1b96dc4f42946bef267f2829ef6953d0962505
User <email@mail.com>
2025-06-07 12:43:08 +0300

    add storage

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -16,9 +15,7 @@ func (handler *MetricsHandler) ServeHTTP(rw http.ResponseWriter, request *http.R
 		http.Error(rw, "Only Post requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println(request.URL.Path)
 	metricsData := strings.Split(request.URL.Path, "/")
-	metricsData = metricsData[2:]
 	if len(metricsData) != 3 {
 		http.Error(rw, "Not all metrics data defined!", http.StatusNotFound)
 		return
@@ -41,14 +38,13 @@ func (handler *MetricsHandler) ServeHTTP(rw http.ResponseWriter, request *http.R
 		http.Error(rw, "Wrong metrics type", http.StatusBadRequest)
 		return
 	}
-
 	rw.WriteHeader(http.StatusOK)
 	rw.Header().Set("Content-type", "text/plain")
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/update/", &MetricsHandler{})
+	mux.Handle("/update/", http.StripPrefix("/update/", &MetricsHandler{}))
 	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
 		panic("Error start server")
 	}
