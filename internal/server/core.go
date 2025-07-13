3ecda62f750b308bb64ab983ba4b2de624ec69dc
User <email@mail.com>
2025-07-13 18:06:20 +0300

    return old values

--- internal/server/core.go
+++ internal/server/core.go
@@ -28,7 +28,7 @@ func newServerConfig() *ServerConfig {
 	serverFlags.UintVar(&serverConfig.StoreInterval, "i", 10, "store interval in seconds. default 300.")
 	serverFlags.StringVar(&serverConfig.StorageDumpPath, "f", "", "path to file for storage dump. Default empty and disable.")
 	serverFlags.StringVar(&serverConfig.SQLDBAddress, "d", "", "DB connection string. Default - empty and disable.")
-	//serverFlags.StringVar(&serverConfig.SQLDBAddress, "d", "host=192.168.1.46 user=bob password=secret dbname=metrics sslmode=disable", "DB connection string. Default - empty and disable.")
+	//serverFlags.StringVar(&serverConfig.SQLDBAddress, "d", "host=localhost user=metricsuser password=secret dbname=metrics sslmode=disable", "DB connection string. Default - empty and disable.")
 	serverFlags.BoolVar(&serverConfig.IsDumpRestore, "r", false, "use dump for restore storage state")
 	serverFlags.Parse(os.Args[1:])
 
